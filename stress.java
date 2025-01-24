f750a13f76f7c7fe0ece62b01f9306fa06187145
TokynBlast <134459455+TokynBlast@users.noreply.github.com>
2025-01-24 11:50:26 -0800

    Finished Interactive UI

--- stress.java
+++ stress.java
@@ -1,3 +1,6 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 public class Main {
@@ -15,7 +18,7 @@ public class Main {
         textField.addFocusListener(new java.awt.event.FocusListener() {
             @Override
             public void focusGained(java.awt.event.FocusEvent e) {
-                if (textField.getText().equals(placeHolder))) {
+                if (textField.getText().equals(placeHolder)) {
                     textField.setForeground(java.awt.Color.WHITE);
                     textField.setText("");
                 }
@@ -23,7 +26,7 @@ public class Main {
 
             @Override
             public void focusLost(java.awt.event.FocusEvent e) {
-                if (textField.getText().equals(""))) {
+                if (textField.getText().equals("")) {
                     textField.setForeground(java.awt.Color.GRAY);
                     textField.setText(placeHolder);
                 }
@@ -31,10 +34,19 @@ public class Main {
         });
     }
 
+    public static void Start(String ip, String PKSize, Integer Time, Integer Delay) {
+        // Start the network stress test
+    }
+
+    public static void Stop() {
+        // Stop the network stress test
+    }
+
     public static void main(String[] args) {
         JFrame window = new JFrame();
         window.setTitle("Javus - Java Network Stressor - v0.0.1");
         window.setVisible(true);
+        window.setResizable(false);
         window.setSize(500, 500);
         window.setLayout(null);
 
@@ -43,15 +55,36 @@ public class Main {
         JTextField Time = new JTextField();
         JTextField Delay = new JTextField();
 
-        styleTextField(IP,         10,  100, 140, 25, "IPv4 Address", "192.168.1.1");
-        styleTextField(PacketSize, 165, 100, 50, 25, "Packet Size (Kilabytes)", "65");
-        styleTextField(Time,       230, 100, 50, 25, "Time (Seconds)", "60");
-        styleTextField(Delay,      295, 100, 70, 25, "Delay (Milliseconds)", "1000");
+        JButton BeginEnd = new JButton();
+
+        BeginEnd.setText("Begin");
+        BeginEnd.setBounds(385, 100, 90, 25);
+
+        BeginEnd.addActionListener(new ActionListener()  {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (BeginEnd.getText().equals("Begin")) {
+                    BeginEnd.setText("Stop");
+                    Start(IP.getText(), PacketSize.getText(), Integer.parseInt(Time.getText()), Integer.parseInt(Delay.getText()));
+                }
+                else if (BeginEnd.getText().equals("Stop")) {
+                    BeginEnd.setText("Begin");
+                    Stop();
+                }
+            }
+        });
+        
+        styleTextField(IP,         10,  100, 140, 25, "IPv4 adress to send packets", "192.168.1.1");
+        styleTextField(PacketSize, 165, 100, 50, 25,  "Size of packets to send in kB", "65");
+        styleTextField(Time,       230, 100, 50, 25,  "Amount of time to send packets for in seconds", "60");
+        styleTextField(Delay,      295, 100, 70, 25,  "Delay between packets in seconds", "1000");
 
         window.getContentPane().setBackground(new java.awt.Color(247, 183, 21));
         window.add(IP);
         window.add(PacketSize);
         window.add(Time);
         window.add(Delay);
+        
+        window.add(BeginEnd);
     }
 }
